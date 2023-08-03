f3137b2b7c9844b08d5fc5732fb595c10d0d7ebd
TarasStefANYUK1996 <uu254256@gmail.com>
2023-08-04 00:54:07 +0300

    fd4f

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
+const btn = $(".js-header");
 function toggleMenu() {
-  const btn = document.querySelector(".header");
-  btn.classList.toggle("_active");
+  btn.toggleClass("_active");
   $("body").toggleClass("lock");
 }
+function toggleLabel(e) {
+  const input = document.getElementById(e.target.id);
+  const label = e.target.closest(".form-group").querySelector(".label-js");
+
+  if (input.value.trim() !== "") {
+    label.style.opacity = "0";
+  } else {
+    label.style.opacity = "1";
+  }
+}
+const labelAll = document.querySelectorAll(".label-js");
+const addIn = () => {
+  labelAll.forEach((label) => {
+    let input = document.querySelector("#email");
+
+    if (input.value.trim() !== "") {
+      label.style.opacity = "0";
+    } else {
+      label.style.opacity = "1";
+    }
+  });
+};
 
 $(document).ready(function () {
+  addIn();
+  let inputElements = document.querySelectorAll("input.required");
+  const textAr = document.querySelector("#message");
+  inputElements = Array.from(inputElements);
+  inputElements.push(textAr);
+
+  // Додаємо обробник події "input" до кожного знайденого елемента
+  inputElements.forEach(function (element) {
+    element.addEventListener("input", function (e) {
+      toggleLabel(e);
+    });
+  });
+
   $('a[href^="#"]').bind("click", function (e) {
     let anchor = $(this);
     $("html, body")
@@ -15,18 +50,15 @@ $(document).ready(function () {
         },
         800
       );
-    const btn = document.querySelector(".header");
-    btn.classList.remove("_active");
+
+    btn.removeClass("_active");
 
     $("body").removeClass("lock");
     e.preventDefault();
   });
-  return false;
-});
-$(document).ready(function () {
   let $currentSlide = $(".current-slide");
   let $totalSlides = $(".total-slides");
-  var $gallery__img = $(".gallery__img");
+  let $gallery__img = $(".js-slick-initialization");
   let $prevButton = $(".arrow__left");
   let $nextButton = $(".arrow__right");
 
@@ -58,7 +90,7 @@ $(document).ready(function () {
     }
   );
 
-  $(".gallery__img").slick({
+  $(".js-slick-initialization").slick({
     autoplay: false,
     autoplaySpeed: 2000,
     dots: false,
